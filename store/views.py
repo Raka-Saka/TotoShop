1d8ff14906b6df4dab83f5f8b47f5e1ca8fff10d
Raka-Saka <83698109+Raka-Saka@users.noreply.github.com>
2022-01-25 16:33:45 +0900

    cart modifs

--- store/views.py
+++ store/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 
@@ -5,6 +6,7 @@ from cart.models import CartItem
 from cart.views import _cart_id
 from .models import Product
 from category.models import Category
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 
 # Create your views here.
@@ -16,10 +18,17 @@ def store(request, category_slug=None):
     if category_slug is not None:
         categories = get_object_or_404(Category, slug=category_slug)
         products = Product.objects.filter(category=categories, is_available=True)
+
+        paginator = Paginator(products, 6)
+        page = request.GET.get('page')
+        paged_products = paginator.get_page(page)
         product_count = products.count()
 
     else:
-        products = Product.objects.all().filter(is_available=True)
+        products = Product.objects.all().filter(is_available=True).order_by('id')
+        paginator = Paginator(products, 6)
+        page = request.GET.get('page')
+        paged_products = paginator.get_page(page)
         product_count = products.count()
 
     context = {
@@ -42,3 +51,15 @@ def product_detail(request, category_slug, product_slug):
         'in_cart': in_cart,
     }
     return render(request, 'store/product_detail.html', context)
+
+def search(request):
+    if 'keyword' in request.GET:
+        keyword = request.GET ['keyword']
+        if keyword:
+            products = Product.objects.order_by('-creation_date').filter(Q(description__icontains=keyword)| Q(product_name__icontains=keyword))
+            product_count = products.count()
+    context = {
+        'products': products,
+        'product_count': product_count,
+    }
+    return render(request, 'store/store.html', context)
